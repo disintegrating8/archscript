7e11253c6c2e648ee0c8f36d3e64615c6a2472f6
disintegrating8 <disintegrating8@pm.me>
2025-04-27 18:56:42 +0900

    Update fcitx-setup.sh

--- fcitx-setup.sh
+++ fcitx-setup.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
+ENV_FILE="$HOME/.pam_environment"
+XPROFILE_FILE="$HOME/.xprofile"
+ENV_CONFIG=("INPUT_METHOD=fcitx5" "GTK_IM_MODULE=fcitx5" "QT_IM_MODULE=fcitx5" "XMODIFIERS=@im=fcitx5")
+
 source utils.sh
 source packages.conf
 
 echo "Installing fcitx5..."
 install_packages "${FCITX[@]}"
 
-configure_environment() {
-    local ENV_FILE="$HOME/.pam_environment"
-    local XPROFILE_FILE="$HOME/.xprofile"
-    local ENV_CONFIG=("INPUT_METHOD=fcitx5" "GTK_IM_MODULE=fcitx5" "QT_IM_MODULE=fcitx5" "XMODIFIERS=@im=fcitx5")
-
-    echo "Configuring environment variables in $ENV_FILE..."
-    for config in "${ENV_CONFIG[@]}"; do
-        if ! grep -q "$config" "$ENV_FILE" 2>/dev/null; then
-            echo "$config" >> "$ENV_FILE"
-        fi
-    done
-
-    echo "Ensuring fcitx5 starts in $XPROFILE_FILE..."
-    if ! grep -q "fcitx5" "$XPROFILE_FILE" 2>/dev/null; then
-        echo 'fcitx5 &' >> "$XPROFILE_FILE"
+echo "Configuring environment variables in $ENV_FILE..."
+for config in "${ENV_CONFIG[@]}"; do
+    if ! grep -q "$config" "$ENV_FILE" 2>/dev/null; then
+        echo "$config" >> "$ENV_FILE"
     fi
-}
+done
+
+echo "Ensuring fcitx5 starts in $XPROFILE_FILE..."
+if ! grep -q "fcitx5" "$XPROFILE_FILE" 2>/dev/null; then
+    echo 'fcitx5 &' >> "$XPROFILE_FILE"
+fi
